a4f0b31f13a2bb6642e3ef5e99aa697a34cc53f5
Ankit <ankit.mittal@barraiser.com>
2023-07-19 21:43:08 +0530

    database change

--- config/db.js
+++ config/db.js
@@ -3,9 +3,8 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const con = await mongoose.connect(process.env.MONGO_URI, {
-      userNewUrlPaser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
     });
 
     console.log(`Mongodb Connect: ${con.connection.host}`);
